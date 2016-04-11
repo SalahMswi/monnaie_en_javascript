69eea8b8d31525d717bfadc650b521fbd295e9d1
SalahMswi <salah.almoussawi@viseo.com>
2016-04-11 17:06:33 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 
+
 describe('renvoi_monnaie_map', function () {
     it('elle doit retourner la monnaie', function () {
+       var maptest=remplir_caisse(3,5,2,5,2,10);
+
         var testmp= [];
         testmp.push(2,4);
         testmp.push(1,5);
 
-
-       expect(testmp).toEqual(renvoi_monnaie_map(7.10,remplir_caisse(3,5,2,5,2,10)));
+       expect(testmp).toEqual(renvoi_monnaie_map(9.20));
+        expect(testmp).toEqual(renvoi_monnaie_map1(9.20,maptest));
 
     });
 });
