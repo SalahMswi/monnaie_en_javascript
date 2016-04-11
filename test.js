7a74c86f98e9277fb60d6352bb699776792c4679
SalahMswi <salah.almoussawi@viseo.com>
2016-04-11 16:08:53 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-/*
-function remplir_caisse(p2,p1,p50c,p20c,p10c){
-    var mp_de_monnaies=[];
-    mp_de_monnaies.push({piece:2,nb:p2});
-    mp_de_monnaies.push({piece:1,nb:p1});
-    mp_de_monnaies.push({piece:0.5,nb:p50c});
-    mp_de_monnaies.push({piece:0.2,nb:p20c});
-    mp_de_monnaies.push({piece:0.1,nb:p10c});
-    console.log(mp_de_monnaies);
-    return mp_de_monnaies;
-}
-*/
+
 describe('renvoi_monnaie_map', function () {
     it('elle doit retourner la monnaie', function () {
         var testmp= [];
